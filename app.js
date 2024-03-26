e3ad6a8fbba9fff77c0063c1585116d419733f32
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-03-27 00:08:32 +0100

    limits set

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const names = document.querySelectorAll('.above input')
 const nums = document.querySelectorAll('.nums input')
 const cvc = document.querySelector('.below label input')
 const err = document.querySelector('#error')
+const err2 = document.querySelector('#error2')
+const label = document.querySelector('.below label')
 
 const card_cvc = document.querySelector('#cvc')
 const card_name = document.querySelector('#name')
@@ -37,25 +39,47 @@ names[1].addEventListener('keyup', function (e){
   else err.style.display = 'block'
 })
 
+const err2Block = () =>{
+  err2.style.display = 'block'
+  label.style.alignSelf = 'flex-start'
+}
+const err2none = () =>{
+  err2.style.display = 'none'
+  label.style.alignSelf = 'flex-end'
+}
+
 nums[0].addEventListener('focusout', function (){
   if(nums[0].value.length == 0) document.querySelectorAll('#base_date span')[0].textContent = '00'
 })
 nums[0].addEventListener('keyup', function (e){
-  document.querySelectorAll('#base_date span')[0].textContent = nums[0].value
-  console.log(nums[0].value)
+  if(nums[0].value.length <= 2){
+    err2none()
+    document.querySelectorAll('#base_date span')[0].textContent = nums[0].value
+    console.log(nums[0].value)
+  }
+  else err2Block()
 })
 nums[1].addEventListener('focusout', function (){
   if(nums[1].value.length == 0) document.querySelectorAll('#base_date span')[1].textContent = '00'
 })
 nums[1].addEventListener('keyup', function (e){
-  document.querySelectorAll('#base_date span')[1].textContent = nums[1].value
-  console.log(nums[1].value)
+  if(nums[1].value.length <= 2){
+    err2none()
+    document.querySelectorAll('#base_date span')[1].textContent = nums[1].value
+    console.log(nums[1].value)
+  }
+  else err2Block()
 })
 
 cvc.addEventListener('focusout', function (){
   if(cvc.value.length == 0) document.querySelector('#cvc').textContent = '000'
 })
 cvc.addEventListener('keyup', function (e){
-  document.querySelector('#cvc').textContent = cvc.value
-  console.log(cvc.value)
+  if(cvc.value.length <= 3){
+    err2none()
+    document.querySelector('#cvc').textContent = cvc.value
+    console.log(cvc.value)
+  }
+  else err2Block()
 })
+console.log('loop')
