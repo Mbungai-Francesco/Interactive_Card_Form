1e54e298e07475e002b5eb142970bd9d8df37a44
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-03-28 14:03:54 +0100

    web design done

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ const card_name = document.querySelector('#name')
 const card_num = document.querySelector('#card_num')
 const card_date = document.querySelectorAll('#base_date span')
 
+const errBlock = (...ele) =>{
+  ele[0].style.display = 'block'
+  ele[1].style.alignSelf = 'flex-start'
+  ele[2].style.borderColor = 'red'
+}
+const errNone = (...ele) =>{
+  ele[0].style.display = 'none'
+  ele[1].style.alignSelf = 'flex-end'
+  ele[2].style.borderColor = gray
+}
+const checkDate = (m,y) =>{
+  console.log(typeof m)
+  // var date = moment(`20${y}-0${m}`);
+  // console.log(date)
+  date = new Date(`20${y}-0${m}`)
+  console.log(date)
+  if (isNaN(date.getTime()) || date.getTime() < 0) return false
+  else return true
+}
+
+const  formatCardNumber = (input) => {
+  const cleanedInput = input.value.replace(/\D/g, "");
+  const regex = /^(\d{4})(\d{4})(\d{4})(\d{4})$/;
+  const formatted = cleanedInput.replace(regex, "$1 $2 $3 $4");
+  return formatted;
+}
+
 names[0].addEventListener('focusout', function (){
   if(names[0].value.length == 0) document.querySelector('#name').textContent = 'Jane Appleseed'
 })
@@ -38,32 +65,13 @@ names[1].addEventListener('keyup', function (e){
       }
     }
     document.querySelector('#card_num').textContent = taker
+    // names[1].value = formatCardNumber(names[1])
   }
   else {
     err.style.display = 'block'
     names[1].style.borderColor = 'red'
   }
 })
-
-const errBlock = (...ele) =>{
-  ele[0].style.display = 'block'
-  ele[1].style.alignSelf = 'flex-start'
-  ele[2].style.borderColor = 'red'
-}
-const errNone = (...ele) =>{
-  ele[0].style.display = 'none'
-  ele[1].style.alignSelf = 'flex-end'
-  ele[2].style.borderColor = gray
-}
-const checkDate = (m,y) =>{
-  console.log(typeof m)
-  // var date = moment(`20${y}-0${m}`);
-  // console.log(date)
-  date = new Date(`20${y}-0${m}`)
-  console.log(date)
-  if (isNaN(date.getTime()) || date.getTime() < 0) return false
-  else return true
-}
 
 nums[0].addEventListener('focusout', function (){
   if(nums[0].value.length == 0) document.querySelectorAll('#base_date span')[0].textContent = '00'
@@ -72,6 +80,7 @@ nums[0].addEventListener('keyup', function (e){
   if(nums[0].value.length <= 2){
     errNone(err2,label,nums[0])
     document.querySelectorAll('#base_date span')[0].textContent = nums[0].value
+    nums[0].value = formatCardNumber(nums[0])
     console.log(nums[0].value)
   }
   else errBlock(err2,label,nums[0])
@@ -84,6 +93,7 @@ nums[1].addEventListener('keyup', function (e){
   if(nums[1].value.length <= 2){
     errNone(err2,label,nums[1])
     document.querySelectorAll('#base_date span')[1].textContent = nums[1].value
+    nums[1].value = formatCardNumber(nums[1])
     console.log(nums[1].value)
   }
   else errBlock(err2,label,nums[1])
@@ -96,6 +106,7 @@ cvc.addEventListener('keyup', function (e){
   if(cvc.value.length <= 3){
     errNone(err2,label,cvc)
     document.querySelector('#cvc').textContent = cvc.value
+    cvc.value = formatCardNumber(cvc)
     console.log(cvc.value)
   }
   else errBlock(err2,label,cvc)
